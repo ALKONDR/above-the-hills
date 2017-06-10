6e4314d1729dace8786f5a777ceb0b5378d408b7
ALKONDR <kondrashkin.98@mail.ru>
2017-06-10 03:11:33 +0300

    navigation basics implemented

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Nav from './presentations/Nav';
+
+// const Router = require('react-router-dom').BrowserRouter;
+// const Route = require('react-router-dom').Route;
+// const Switch = require('react-router-dom').Switch;
 
 class App extends React.PureComponent {
   render() {
     return (
       <div className="container">
-        <h1> Our cool app </h1>
+        <Nav />
       </div>
     );
   }
