359a5735b3f91d5b43594908241f3c445d8d9e2b
Nikita Tarasov <mail@ntarasov.ru>
2017-06-11 11:53:37 +0300

    no message

--- webserver/sqlserver.py
+++ webserver/sqlserver.py
@@ -12,7 +12,48 @@ connection_config = {
 
 cnx = mysql.connector.connect(**connection_config)
 
+def all_categories():
+    cursor = cnx.cursor()
+    query = 'SELECT * FROM meme_category'
+    cursor.execute(query)
+
+    elems = []
+    result = []
+    
+    for elem in cursor:
+        elems.append(elem)
+        
+    cursor.close()
+    print(json.dumps(elems))
+
+    for elem in elems:
+        print(elem)
+            
+        c = cnx.cursor()
+        q = 'SELECT difference FROM changes WHERE meme_id = ' + str(elem[0]) + ' ORDER BY timestamp DESC;'
+        print("all_categories(): query:")
+        print(q)
+        c.execute(q)
+        diffval = c.fetchone()
+
+        if diffval is None:
+            diffval = [0]
+            
+        result.append({
+            "id": elem[0],
+            "name": elem[1],
+            "price": elem[2],
+            "popularity": elem[3],
+            "diff": diffval[0]
+        })
+        c.close()
+    return json.dumps(result)
+    cursor.close()
+    
+
 def categories(name):
+    if name == "":
+        return all_categories()
     cursor = cnx.cursor()
     print("recv cursor")
     query = ('SELECT * FROM meme_category WHERE name = "' + str(name) + '";')
